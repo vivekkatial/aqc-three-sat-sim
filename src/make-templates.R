5c2da2c171a90d56909df7e8dbe3274f38226dc6
Vivek Katial <vivekkatial@gmail.com>
2019-10-11 19:26:33 +1100

    Added files

---
+++ src/make-templates.R
@@ -0,0 +1,12 @@
+###############################################################################
+# Generates 3 SAT Template Files for experimentation
+#
+# Author: Vivek Katial
+# Created 2019-10-11 19:17:10
+###############################################################################
+
+# Import yaml file
+library(yaml)
+
+# Read in file
+param_spec <- read_yaml("params/params_spec.yml")
