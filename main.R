b318fe1dbb6ab86900e3df04f07352805dd88aa0
Vivek Katial <vivekkatial@gmail.com>
2019-11-04 19:25:53 +1100

    added mlflow integration

--- main.R
+++ main.R
@@ -34,19 +34,27 @@ params <- extract_params(exp_param_file)
 # Setup MLflow
 loginfo("Setting up MLFlow")
 
+#  Initialise MLFlow Client
+loginfo("Initialising MLFlow with tracking URI: '%s'", params$experiment$`tracking-uri`)
 mlflow_set_tracking_uri(params$experiment$`tracking-uri`)
+mlflow_client <- mlflow:::mlflow_client(tracking_uri = params$experiment$`tracking-uri`)
+
+# Check if experiment already  present (if not create one)
+if (!(params$experiment$name %in% mlflow:::mlflow_list_experiments()$name)) {
+  logwarn("Experiment %s not found, creating...", params$experiment$name)
+  mlflow::mlflow_create_experiment(params$experiment$name, params$experiment$name)
+} else {
+  loginfo("Experiment %s already present, running run with configuration %s", params$experiment$name, exp_param_file)
+}
+
+
+
 
 # Start mlflow run
-mlflow_start_run(
-#  run_uuid = as.character(exp_param_file)#,
-#  experiment_id = as.character(params$experiment$name),
-#  source_version = as.character(system("git rev-parse HEAD", intern=TRUE))
-)
+mlflow_start_run()
 
 mlflow_log_artifact(exp_param_file)
-mlflow_log_param("foo", 42)
-
-mlflow_end_run()
+mlflow_log_param("file_name", exp_param_file)
 
 # Source in all scripts
 source_exp_scripts(params)
@@ -79,7 +87,7 @@ phi_T <- evolve_quantum_system(d_hamils, params$build_hamiltonians$params)
 loginfo("Solved system for T='%s'", params$build_hamiltonians$params$time_T)
 
 # Solving for Entanglement
-d_entanglement <- calculate_system_entanglement(d_hamils)
+# d_entanglement <- calculate_system_entanglement(d_hamils)
 
 
 # Generate PDF ------------------------------------------------------------
@@ -100,4 +108,10 @@ loginfo("Plotting Energy Gap")
 p_energy_gap <- d_hamils %>% 
   plot_energy_gap()
 
+
+# Ending Experiment -------------------------------------------------------
+
+# Ending run
+mlflow_end_run()
 loginfo("Experiment Complete!")
+
