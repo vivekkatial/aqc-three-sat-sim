c69210031262e6cedbfab58eed06cce823316d0d
vivekkatial <vivekkatial@gmail.com>
2019-11-26 12:34:36 +1100

    Added entanglement code and code for renv

--- src/measure-quantum/measure-entanglement.R
+++ src/measure-quantum/measure-entanglement.R
@@ -5,18 +5,65 @@
 # Created 2019-11-11 11:16:58
 ###############################################################################
 
-example_ham <- matrix(
-  c(1,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,1,0,0,0,0,
-    0,0,0,0,0,0,0,0,
-    0,0,0,0,0,1,0,0,
-    0,0,0,0,0,0,1,0,
-    0,0,0,0,0,0,0,1),
-  nrow = 8,
-  ncol = 8
-)
+#' This function builds the density matrix for a given state_vector
+#' @param .phi The state vector in consideration
+#' @param .n_qubits **Optional** The number of qubits of the system
+#' @return state_matrix 
+build_state_matrix = function(.phi, .n_qubits){
+  
+  # Get dimensions
+  matrix_dimensions = get_dims_density_matrix(.n_qubits)
+  
+  # Build state matrix
+  state_matrix = .phi %>% 
+    matrix(nrow = matrix_dimensions$n_row, ncol = matrix_dimensions$n_col)
+  
+  # Return state matrix
+  state_matrix
+}
 
-decomposed_system <- RQEntangle::schmidt.decompose(example_ham)
+# Find midpoint for bipartite system
+#' @param .n_qubits Parameter for number of qubits
+#' @return A list containing number of cols and number of 
+get_dims_density_matrix = function(.n_qubits){
+  
+  # TODO: Check for integer value of .n_qubits
+
+  # Check if n_qubits odd or even
+  if (n_qubits %% 2 == 0) {
+    n_row = (2^(n_qubits))/2
+    n_col = (2^(n_qubits))/2
+  } else {
+    n_row = (2^n_qubits)/2
+    n_col = (2^(n_qubits - 1))/2
+  }
+  
+  # Return params
+  list(
+    n_row = n_row,
+    n_col = n_col
+  )
+}
+
+
+#' Compute Entanglement of a state vector
+#' @param .phi state vector
+#' @param .n_qubits Parameter for number of qubits
+#' @return shannon entropy
+calculate_entanglement = function(.phi, .n_qubits){
+  
+  # Build State Matrix for phi
+  state_matrix = build_state_matrix(.phi, .n_qubits)
+  
+  # Apply Singular Value Decomposition on state matrix
+  decomposed_system = svd(state_matrix)
+  
+  # Extract correlations
+  p_decomp = (decomposed_system$d)^2
+  
+  # Find shannon entropy
+  shannon_entropy = sum(-p_decomp * log2(p_decomp))
+  
+  shannon_entropy
+}
 
