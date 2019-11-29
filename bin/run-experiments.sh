2d140e906fd35b7e846efbbfaadf48b2aff2e95d
vivekkatial <vivekkatial@gmail.com>
2019-11-29 18:09:19 +1100

    WIP experiments

--- bin/run-experiments.sh
+++ bin/run-experiments.sh
@@ -1,16 +1,28 @@
-# List ready experiments
+#!/bin/bash
 
+# --------------------------------------------
+# This script spins up diff experiments
+#
+# Author: Vivek Katial
+# --------------------------------------------
+
+# Set env vars
+source bin/set-environment-variables.sh
+
+trap "exit" INT
+
+# Set the directory in which experiments parameter files stored
 export EXPERIMENT_FILE_DIR=$S3_BUCKET/$EXPERIMENT_NAME/ready/
-# aws s3 ls $EXPERIMENT_FILE_DIR --endpoint-url=$MLFLOW_S3_ENDPOINT_URL
-
-# for file in "$EXPERIMENT_FILE_DIR"*; do
-#   echo "Running Experiment on $file"
-#   # Run Script
-#   # Rscript main.R $file
-# done
 
 for file in $(aws s3 ls $EXPERIMENT_FILE_DIR --endpoint-url=$MLFLOW_S3_ENDPOINT_URL | awk '{print $4}'); do
-    echo "Running experiment on $file"
-    # Pull file into local directory
-    aws s3 
+	 # Copy file
+     echo "Copying experiment $EXPERIMENT_FILE_DIR$file params/ready/."
+     # Pull file into local directory
+     aws s3 cp $EXPERIMENT_FILE_DIR$file params/ready/ --endpoint-url=$MLFLOW_S3_ENDPOINT_URL
+
+     # Experiment name
+     experiment_name=${file::${#file}-4}
+     # Run experiment as an instance of the singularity container
+     sbatch bin/run_experiment.slurm --output=$file.log 
+
 done
