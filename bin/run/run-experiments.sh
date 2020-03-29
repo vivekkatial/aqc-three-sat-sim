650b5d6a3665b90653016557d2b88a13bc89ed69
Vivek Katial <vivekkatial@gmail.com>
2020-03-29 18:43:54 +1300

    Adding fix for dat path

--- bin/run/run-experiments.sh
+++ bin/run/run-experiments.sh
@@ -12,7 +12,7 @@ set -e
 source bin/init/set-environment-variables.sh
 
 # Set the directory in which experiments parameter files stored
-export EXPERIMENT_FILE_DIR=$S3_BUCKET/$EXPERIMENT_NAME/ready/$DATE_PATH
+export EXPERIMENT_FILE_DIR=$S3_BUCKET/$EXPERIMENT_NAME/ready/
 echo "$EXPERIMENT_FILE_DIR"
 
 for file in $(aws s3 ls $EXPERIMENT_FILE_DIR --endpoint-url=$MLFLOW_S3_ENDPOINT_URL | awk '{print $4}'); do
