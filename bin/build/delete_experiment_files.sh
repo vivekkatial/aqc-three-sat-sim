c71bdd48fadfc0693b011534f5cfa17714b67566
Vivek Katial <vivekkatial@gmail.com>
2020-03-29 21:58:16 +1300

    Submitting delete script

---
+++ bin/build/delete_experiment_files.sh
@@ -0,0 +1,15 @@
+module load awscli/1.16.67-GCC-6.2.0
+
+# Set env vars
+source bin/init/set-environment-variables.sh
+
+# Set the directory in which experiments parameter files stored
+export EXPERIMENT_FILE_DIR=$S3_BUCKET/$EXPERIMENT_NAME/ready/
+echo "$EXPERIMENT_FILE_DIR"
+
+# Remove all scripts
+read -r -p "Are you sure you wish to delete $EXPERIMENT_FILE_DIR? [Y/n]" response
+response=${response,,} # tolower
+if [[ $response =~ ^(yes|y| ) ]] || [[ -z $response ]]; then
+    aws s3 rm --recursive $EXPERIMENT_FILE_DIR --endpoint-url=$MLFLOW_S3_ENDPOINT_URL    
+fi
