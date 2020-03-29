1b89bd04ba795b39e5ba8480eed796c1998954ad
Vivek Katial <vivekkatial@gmail.com>
2020-03-29 17:50:28 +1300

    Updated run experiments script

--- bin/run/run-experiments.sh
+++ bin/run/run-experiments.sh
@@ -9,10 +9,9 @@
 # Set env vars
 source bin/init/set-environment-variables.sh
 
-trap "exit" INT
-
 # Set the directory in which experiments parameter files stored
 export EXPERIMENT_FILE_DIR=$S3_BUCKET/$EXPERIMENT_NAME/ready/
+echo "$EXPERIMENT_FILE_DIR"
 
 for file in $(aws s3 ls $EXPERIMENT_FILE_DIR --endpoint-url=$MLFLOW_S3_ENDPOINT_URL | awk '{print $4}'); do
      # Copy file
