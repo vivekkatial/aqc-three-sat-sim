01026d55cda5e30df3c8f3ccc9bfb9005e08db95
Vivek Katial <vivekkatial@gmail.com>
2020-04-24 01:35:05 +1200

    Added mlflow analysis stuff

--- analysis/instance-characteristics.R
+++ analysis/instance-characteristics.R
@@ -10,13 +10,19 @@ library(tidyverse)
 
 # Global Vars
 MLFLOW_TRACKING_URI="http://115.146.95.176:5000/"
-EXPERIMENT="three-sat"
+EXPERIMENT="three-sat-usa"
 
 mlflow::mlflow_set_tracking_uri(MLFLOW_TRACKING_URI)
+d_runs <- mlflow_list_run_infos(experiment_id = 2)
 
-mlflow_list_experiments()
-d_runs <- mlflow_list_run_infos(run_view_type = "ACTIVE", experiment_id = 1)
+clean_metric = function(run_id, metric){
+  resp = mlflow_get_metric_history(metric_key = metric, run_id = run_id)
+  resp$value
+}
 
 d_runs %>% 
-  filter(!is.na(end_time)) %>% 
-  mutate(metrics = mlflow::mlflow_get_metric_history)
+  filter(status == "FINISHED") %>% 
+  mutate(run_info = map(run_id, mlflow_get_run)) %>% 
+  clean_runs()
+
+
